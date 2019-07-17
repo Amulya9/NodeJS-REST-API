ae97bf4295b955b306e830f3706fe259edf010fa
Amulya9 <52939163+Amulya9@users.noreply.github.com>
2019-07-17 18:21:36 -0500

    NodeJs and REST API application.

---
+++ config.js
@@ -0,0 +1,21 @@
+var config = {};
+
+config.port = 3000;
+
+//Authentication
+config.auth = false;
+
+//Database
+
+
+config.host='localhost';
+config.database = 'sys';
+config.username = 'root';
+config.password = 'Hemaammu@3';
+config.table_prefix = '';
+
+//Pagination
+config.paginate = true;
+config.page_limit = 10;
+
+module.exports = config;
